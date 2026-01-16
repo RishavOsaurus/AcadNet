143f8748eccdedbab2cc404fc9c719b09adc0688
Rishav Chapagain <gainrishavchap@gmail.com>
2026-01-16 12:26:48 +0545

    Checks

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/',
+  baseURL: 'https://acad-net.vercel.app/api/v1/',
   withCredentials: true,
 });
 
